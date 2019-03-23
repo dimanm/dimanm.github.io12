325344a396229957c63c8b2a42b225be12dcd9dd
your name <you@example.com>
2019-03-23 13:23:11 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//alert('test');
+//console.log('test');
+$('.sl').slick({
+
+	lazyLoad: 'progressive',
+	dots: true,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	speed: 1500,
+	cssEase:'ease',
+	infinity: true,
+	pauseOnFocus: true,
+	pauseOnHover: false,
+	pauseOnDotsHover: true,
+	centerMode: true,
+	centerPadding: '0px'
+});
